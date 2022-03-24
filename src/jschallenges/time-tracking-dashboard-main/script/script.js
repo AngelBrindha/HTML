9256410e4f239075fb3fcaf3dd6824015bf987dc
Angel Brindha S <ange3024@global.chainsys.com>
2022-03-24 22:21:40 +0530

    files modified

--- src/jschallenges/time-tracking-dashboard-main/script/script.js
+++ src/jschallenges/time-tracking-dashboard-main/script/script.js
@@ -1,82 +1,60 @@
-const daily = document.querySelector('#daily');
-const weekly = document.querySelector('#weekly');
-const monthly = document.querySelector('#monthly');
+const dailyBtn = document.querySelector('#daily');
+const weeklyBtn = document.querySelector('#weekly');
+const monthlyBtn = document.querySelector('#monthly');
 
-const workTimeCurrent = document.querySelector('#work-time-current');
-const workTimePrevious = document.querySelector('#work-time-previous');
-const playTimeCurrent = document.querySelector('#play-time-current');
-const playTimePrevious = document.querySelector('#play-time-previous');
-const studyTimeCurrent = document.querySelector('#study-time-current');
-const studyTimePrevious = document.querySelector('#study-time-previous');
-const exerciseTimeCurrent = document.querySelector('#exercise-time-current');
-const exerciseTimePrevious = document.querySelector('#exercise-time-previous');
-const socialTimeCurrent = document.querySelector('#social-time-current');
-const socialTimePrevious = document.querySelector('#social-time-previous');
-const selfCareTimeCurrent = document.querySelector('#selfcare-time-current');
-const selfCareTimePrevious = document.querySelector('#selfcare-time-previous');
+const hours = document.querySelectorAll('.hour');
+const lastTime = document.querySelectorAll('.duration');
 
-daily.addEventListener('click', dailyFunc);
-
-function dailyFunc() {
-  workTimeCurrent.textContent = '5hrs';
-  workTimePrevious.textContent = 'Last Day - 7hrs';
-
-  playTimeCurrent.textContent = '1hrs';
-  playTimePrevious.textContent = 'Last Day - 2hrs';
-
-  studyTimeCurrent.textContent = '0hrs';
-  studyTimePrevious.textContent = 'Last Day - 1hrs';
-
-  exerciseTimeCurrent.textContent = '1hrs';
-  exerciseTimePrevious.textContent = 'Last Day - 1hrs';
-
-  socialTimeCurrent.textContent = '1hrs';
-  socialTimePrevious.textContent = 'Last Day - 3hrs';
-
-  selfCareTimeCurrent.textContent = '0hrs';
-  selfCareTimePrevious.textContent = 'Last Day - 1hrs';
+function dailyData() {
+  fetch(
+    'https://html-delta-gold.vercel.app/src/jschallenges/time-tracking-dashboard-main/data.json'
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      hours.forEach((e, i) => {
+        hours[i].innerHTML = data[i].timeframes.daily.current + 'hrs';
+        lastTime[i].innerHTML =
+          'Last Daily - ' + data[i].timeframes.daily.previous + 'hrs';
+      });
+    });
 }
 
-weekly.addEventListener('click', weeklyFunc);
-
-function weeklyFunc() {
-  workTimeCurrent.textContent = '32hrs';
-  workTimePrevious.textContent = 'Last Week - 36hrs';
-
-  playTimeCurrent.textContent = '10hrs';
-  playTimePrevious.textContent = 'Last Week - 8hrs';
-
-  studyTimeCurrent.textContent = '4hrs';
-  studyTimePrevious.textContent = 'Last Week - 7hrs';
-
-  exerciseTimeCurrent.textContent = '4hrs';
-  exerciseTimePrevious.textContent = 'Last Week - 5hrs';
-
-  socialTimeCurrent.textContent = '5hrs';
-  socialTimePrevious.textContent = 'Last Week - 10hrs';
-
-  selfCareTimeCurrent.textContent = '2hrs';
-  selfCareTimePrevious.textContent = 'Last Week - 2hrs';
+function weeklyData() {
+  fetch(
+    'https://html-delta-gold.vercel.app/src/jschallenges/time-tracking-dashboard-main/data.json'
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      hours.forEach((e, i) => {
+        hours[i].innerHTML = data[i].timeframes.weekly.current + 'hrs';
+        lastTime[i].innerHTML =
+          'Last Weekly - ' + data[i].timeframes.weekly.previous + 'hrs';
+      });
+    });
 }
 
-monthly.addEventListener('click', monthlyFunc);
-
-function monthlyFunc() {
-  workTimeCurrent.textContent = '103hrs';
-  workTimePrevious.textContent = 'Last Month - 128hrs';
-
-  playTimeCurrent.textContent = '23hrs';
-  playTimePrevious.textContent = 'Last Month - 29hrs';
-
-  studyTimeCurrent.textContent = '13hrs';
-  studyTimePrevious.textContent = 'Last Month - 19hrs';
-
-  exerciseTimeCurrent.textContent = '11hrs';
-  exerciseTimePrevious.textContent = 'Last Month - 18hrs';
-
-  socialTimeCurrent.textContent = '21hrs';
-  socialTimePrevious.textContent = 'Last Month - 23hrs';
-
-  selfCareTimeCurrent.textContent = '7hrs';
-  selfCareTimePrevious.textContent = 'Last Month - 11hrs';
+function monthlyData() {
+  fetch(
+    'https://html-delta-gold.vercel.app/src/jschallenges/time-tracking-dashboard-main/data.json'
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      hours.forEach((e, i) => {
+        hours[i].innerHTML = data[i].timeframes.monthly.current + 'hrs';
+        lastTime[i].innerHTML =
+          'Last Monthly - ' + data[i].timeframes.monthly.previous + 'hrs';
+      });
+    });
 }
+
+dailyBtn.addEventListener('click', () => {
+  dailyData();
+});
+
+weeklyBtn.addEventListener('click', () => {
+  weeklyData();
+});
+
+monthlyBtn.addEventListener('click', () => {
+  monthlyData();
+});
