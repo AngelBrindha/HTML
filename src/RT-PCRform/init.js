b43750f0c469c42ce647744868731fd7edcb7cd0
Angel Brindha S <ange3024@global.chainsys.com>
2022-03-31 21:32:20 +0530

    modified

--- src/RT-PCRform/init.js
+++ src/RT-PCRform/init.js
@@ -1,6 +1,7 @@
 import { add } from './element.js';
 import { savedata } from './handler.js';
 import { myFunction } from './function.js';
+import { searchDisplay, searchdata } from './radio.js';
 
 {
   add.addEventListener('click', (e) => {
@@ -11,4 +12,6 @@ import { myFunction } from './function.js';
   }
   savedata();
   myFunction();
+  searchDisplay();
+  searchdata();
 }
