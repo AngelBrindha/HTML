bef1989374ca4330f9d8680143683f9cfe8e1e42
Angel Brindha S <ange3024@global.chainsys.com>
2022-04-01 12:01:57 +0530

    modified

--- src/RT-PCRform/function.js
+++ src/RT-PCRform/function.js
@@ -6,23 +6,23 @@ search.addEventListener('click', (e) => {
 
 export function myFunction() {
   var input, filter, table, tr, txtValue3, td3, j;
-  // var  td1, i, txtValue1;
+  var td1, i, txtValue1;
   input = document.getElementById('search');
   filter = input.value.toUpperCase();
   table = document.getElementById('searchTable');
   tr = table.getElementsByTagName('tr');
 
-  // for (i = 0; i < tr.length; i++) {
-  //   td1 = tr[i].getElementsByTagName('td')[0];
-  //   if (td1) {
-  //     txtValue1 = td1.textContent || td1.innerText;
-  //     if (txtValue1.toUpperCase().indexOf(filter) > -1) {
-  //       tr[i].style.display = '';
-  //     } else {
-  //       tr[i].style.display = 'none';
-  //     }
-  //   }
-  // }
+  for (i = 0; i < tr.length; i++) {
+    td1 = tr[i].getElementsByTagName('td')[0];
+    if (td1) {
+      txtValue1 = td1.textContent || td1.innerText;
+      if (txtValue1.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = '';
+      } else {
+        tr[i].style.display = 'none';
+      }
+    }
+  }
   for (j = 0; j < tr.length; j++) {
     td3 = tr[j].getElementsByTagName('td')[2];
     if (td3) {
