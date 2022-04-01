4f0f3474cf84884d79001651b48c1f081d7fd6b2
Angel Brindha S <ange3024@global.chainsys.com>
2022-04-01 12:35:06 +0530

    modified

--- src/RT-PCRform/search.js
+++ src/RT-PCRform/search.js
@@ -1,11 +1,11 @@
 import { search, table1 } from './element.js';
-
 search.addEventListener('click', (e) => {
   e.preventDefault();
   searchdata();
 });
 
 export function searchdata() {
+  var td3;
   var data = {
     name: document.getElementById('name').value,
     gender: document.querySelector('input[name="gender"]:checked').value,
@@ -19,6 +19,7 @@ export function searchdata() {
     reportNum: document.getElementById('report-num').value,
   };
   console.log(data);
+
   var row = table1.insertRow(-1);
 
   var cell1 = row.insertCell(-1);
@@ -32,15 +33,15 @@ export function searchdata() {
   var cell9 = row.insertCell(-1);
   var cell10 = row.insertCell(-1);
 
-  cell1.innerHTML = data.name;
-  cell2.innerHTML = data.gender;
-  cell3.innerHTML = data.aadhar;
-  cell4.innerHTML = data.dob;
-  cell5.innerHTML = data.city;
-  cell6.innerHTML = data.dot;
-  cell7.innerHTML = data.tot;
-  cell8.innerHTML = data.result;
-  cell9.innerHTML = data.labName;
-  cell10.innerHTML = data.reportNum;
+  cell1.innerHTML = data.name.td3;
+  cell2.innerHTML = data.gender.td3;
+  cell3.innerHTML = data.aadhar.td3;
+  cell4.innerHTML = data.dob.td3;
+  cell5.innerHTML = data.city.td3;
+  cell6.innerHTML = data.dot.td3;
+  cell7.innerHTML = data.tot.td3;
+  cell8.innerHTML = data.result.td3;
+  cell9.innerHTML = data.labName.td3;
+  cell10.innerHTML = data.reportNum.td3;
   table1.appendChild(row);
 }
